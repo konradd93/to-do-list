9254fa79e7030c00851e38cf3be81d5343734e0b
Konrad Depta <konradd93@gmail.com>
2017-08-03 14:01:06 +0200

    Implemented simple services

--- src/main/java/pl/pollub/service/TaskService.java
+++ src/main/java/pl/pollub/service/TaskService.java
@@ -1,8 +1,12 @@
 package pl.pollub.service;
 
-import pl.pollub.model.task.Task;
 import pl.pollub.model.task.DTO.TaskDTO;
+import pl.pollub.model.task.Task;
+
+import java.util.List;
 
 public interface TaskService {
     Task createTask(TaskDTO taskDTO);
+
+    List<Task> getAllTasks();
 }
--- src/main/java/pl/pollub/service/TaskServiceImpl.java
+++ src/main/java/pl/pollub/service/TaskServiceImpl.java
@@ -1,21 +1,30 @@
 package pl.pollub.service;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pl.pollub.model.task.Task;
 import pl.pollub.model.task.DTO.TaskDTO;
+import pl.pollub.model.task.Task;
+import pl.pollub.model.task.converter.TaskDtoToEntityConverter;
 import pl.pollub.repository.TaskRepository;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TaskServiceImpl implements TaskService {
 
-    @Autowired
-    private TaskRepository taskRepository;
+    private final @NonNull TaskRepository taskRepository;
+    private final @NonNull TaskDtoToEntityConverter taskDtoToEntityConverter;
 
     @Override
     public Task createTask(TaskDTO taskDTO) {
-        Task newTask = new Task(taskDTO);
-        taskRepository.save(newTask);
-        return newTask;
+        return taskRepository.save(taskDtoToEntityConverter.convert(taskDTO));
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
     }
 }
